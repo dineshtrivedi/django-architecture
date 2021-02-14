b45a9b30ccdf937e15e855cb38da87562db331f8
Dinesh Trivedi <dinesh.dart@gmail.com>
2021-02-14 10:07:46 +0200

    Initial commit
    - Pyenv
    - Django project
    - Core app
    - Shell helpers

---
+++ README.md
@@ -0,0 +1,15 @@
+# Django Architecture
+
+## Introduction and motivation
+To be done
+
+## Development environment
+### 1. Pyenv
+
+Lets use [pyenv](https://github.com/pyenv/pyenv) to manage our python versions.
+
+Install the prerequisites for your OS by checking [Pyenv - Common build problems](https://github.com/pyenv/pyenv/wiki/Common-build-problems).
+
+Then follow the documentation for installation [here](https://github.com/pyenv/pyenv-installer).
+
+This project uses python version defined in the file [.python-version](.python-version)
---
+++ dev.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+RESTORE='\033[0m'
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+YELLOW='\033[00;33m'
+
+export VENV_BASE=.django_architecture
+export PROJ_BASE="$(dirname "${BASH_SOURCE[0]}")"
+CD=$(pwd)
+cd "$PROJ_BASE"
+export PROJ_BASE=$(pwd)
+cd $CD
+
+. ci/git_aliases.sh
+
+alias manage='python "$PROJ_BASE"/board_manager/manage.py'
+
+devhelp() {
+    echo -e "${GREEN}devhelp${RESTORE}              Prints this ${RED}help${RESTORE}"
+    echo -e ""
+    echo -e "${GREEN}rebuild_virtualenv${RESTORE}           Rebuild virtualenv for this project"
+    echo -e ""
+}
+
+_create_git_aliases() {
+    git config alias.co checkout
+    git config alias.st status
+    git config alias.ci commit
+    git config alias.br branch
+    git config alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+}
+
+echo_red() {
+    echo -e "${RED}$1${RESTORE}";
+}
+
+echo_green() {
+    echo -e "${GREEN}$1${RESTORE}";
+}
+
+echo_yellow() {
+    echo -e "${YELLOW}$1${RESTORE}";
+}
+
+rebuild_virtualenv() {
+    CD=$(pwd)
+    cd "$PROJ_BASE"
+
+    source "$PROJ_BASE/$VENV_BASE/bin/deactivate"
+    rm -rf "$VENV_BASE"
+
+    pyenv install --skip-existing
+
+    python -m venv "$VENV_BASE"
+    source  "$PROJ_BASE/$VENV_BASE/bin/activate"
+
+    pip install --upgrade pip==21.0.1 setuptools==53.0.0
+    pip install -r requirements.txt
+
+    cd $CD
+}
+
+_create_git_aliases
+
+
+PYTHON_VERSION=`cat ./.python-version`
+echo_green "Installing python version: ${PYTHON_VERSION}\n"
+
+pyenv install --skip-existing | exit 0
+
+echo_green "Welcome to django_architecture's dev env"
+echo_green "Hint: autocomplete works for the commands below ;)"
+echo_red   "------------------------------------------------------------------------"
+devhelp
+
+
+if test -f "$PROJ_BASE/$VENV_BASE/bin/activate"; then
+    source "$PROJ_BASE/$VENV_BASE/bin/activate"
+else
+    echo_red "No virtualenv found!"
+    echo_green "Run: rebuild_virtualenv"
+fi
+
